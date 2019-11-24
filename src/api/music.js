64986144122a3ebfa192a688ddb9314e7738b0c0
sascharissling <sascha.rissling@gmail.com>
2019-11-24 21:47:19 +0100

    remove Zeit method for now to make app presentable

--- src/api/music.js
+++ src/api/music.js
@@ -2,8 +2,11 @@
 
 export async function getDiscoverMusic(value) {
   const response = await fetch(
-    `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${value}&api_key=${process.env.REACT_APP_API_KEY}&format=json`
+    `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${value}&api_key=b3bdf0f2b851d1ff6f91dab2a0f1763b&format=json`
   );
   const data = await response.json();
   return data.topartists.artist;
 }
+
+// For When Zeit Secret Works
+// http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${value}&api_key=${process.env.REACT_APP_API_KEY}&format=json
